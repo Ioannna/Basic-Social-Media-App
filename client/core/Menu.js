5a9f9dbb1d39f9504c9e5a537efc800858d15c25
Ioana Biris <67058868+Ioannna@users.noreply.github.com>
2025-05-30 14:06:20 +0300

    Change Title of the app

--- client/core/Menu.js
+++ client/core/Menu.js
@@ -18,7 +18,7 @@ const Menu = withRouter(({history}) => (
   <AppBar position="static">
     <Toolbar>
       <Typography variant="h6" color="inherit">
-        HERE
+         explore IT
       </Typography>
       <Link to="/">
         <IconButton aria-label="Home" style={isActive(history, "/")}>
